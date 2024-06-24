21f563f81743e9f6a77205d97457160fe12f79d5
Ruben Zafra Traver <rubenreamps@gmail.com>
2024-06-25 00:08:16 +0200

    ✨ Show first pizza text and image

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,35 +1,39 @@
-import { StyleSheet } from "react-native";
-
-import EditScreenInfo from "@/src/components/EditScreenInfo";
+import { Image, StyleSheet } from "react-native";
 import { Text, View } from "@/src/components/Themed";
+import Colors from "@/src/constants/Colors";
+import products from "@/assets/data/products";
+
+const product = products[0];
 
 export default function TabOneScreen() {
+  const { name, price, image } = product;
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One Pepe</Text>
-      <View
-        style={styles.separator}
-        lightColor='#eee'
-        darkColor='rgba(255,255,255,0.1)'
-      />
-      <EditScreenInfo path='app/(tabs)/index.tsx' />
+      <Image style={styles.image} source={{ uri: image }} />
+      <Text style={styles.title}>{name}</Text>
+      <Text style={styles.price}>${price}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 20,
   },
   title: {
     fontSize: 20,
-    fontWeight: "bold",
+    fontWeight: "600",
+    marginVertical: 10,
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
+  price: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: Colors.light.tint,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
   },
 });
