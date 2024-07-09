c0170283f3f1b7cd697fe9422dbc2902bb59d10a
Ruben Zafra Traver <rubenreamps@gmail.com>
2024-07-09 15:56:01 +0200

    🔧 Set up supabase

---
+++ src/lib/supabase.ts
@@ -0,0 +1,28 @@
+import "react-native-url-polyfill/auto";
+import * as SecureStore from "expo-secure-store";
+import { createClient } from "@supabase/supabase-js";
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key);
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value);
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key);
+  },
+};
+
+const supabaseUrl = "https://khzaaqhakkzwlwqpeoot.supabase.co";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtoemFhcWhha2t6d2x3cXBlb290Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjA1MzMxMTMsImV4cCI6MjAzNjEwOTExM30.5H91RFEBKap9o2yzPZ9HXLHhk8H8U2Q5g2497gRnVRY";
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: ExpoSecureStoreAdapter as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
